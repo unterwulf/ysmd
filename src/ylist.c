b6952b7b69b15843eebb8c691671f71e49157b17
Vitaly Sinilin <vs@kp4.ru>
2009-10-24 23:19:43 +0400

    import source tree state on 20 Nov 2008 (phase 2)

--- src/ylist.c
+++ src/ylist.c
@@ -1,51 +1,25 @@
-/*
--======================== ysmICQ client ============================-
-        Having fun with a boring Protocol
--========================= YSM_Lists.c =============================-
-
-YSM (YouSickMe) ICQ Client. An Original Multi-Platform ICQ client.
-Copyright (C) 2002 rad2k Argentina.
-
-YSM is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-For Contact information read the AUTHORS file.
-
-*/
-
 #include "ysm.h"
-#include "lists.h"
+#include "ylist.h"
 #include "wrappers.h"
 #include "toolbox.h"
 
-void freelist(dl_list_t *list)
+void freeList(list_t *list)
 {
-    dl_list_node_t *suc, *node = list->start;
+    list_node_t *suc, *node = list->start;
 
     while (list->length > 0)
     {
         if (!node) break;
         suc = node->suc;
-        list_delete(list, node);
+        deleteListNode(list, node);
         node = suc;
     }
 }
 
-dl_list_node_t * list_insert_after(
-    dl_list_t      *list,
-    dl_list_node_t *node,
-    dl_list_node_t *pre)
+list_node_t * insertListNodeAfter(
+    list_t      *list,
+    list_node_t *node,
+    list_node_t *pre)
 {
     if (list->start == NULL)
     {
@@ -65,7 +39,7 @@ dl_list_node_t * list_insert_after(
     return node;
 }
 
-dl_list_node_t * list_unshift(dl_list_t *list, dl_list_node_t *node)
+list_node_t * unshiftListNode(list_t *list, list_node_t *node)
 {
     if (list->start == NULL)
     {
@@ -87,14 +61,14 @@ dl_list_node_t * list_unshift(dl_list_t *list, dl_list_node_t *node)
     return node;
 }
 
-void list_delete(dl_list_t *list, dl_list_node_t *node)
+void deleteListNode(list_t *list, list_node_t *node)
 {
     if (node != list->start)
     {
         node->pre->suc = node->suc;
 
         // could be the last element in the list..therefore suc might be null.
-        if (node->suc != 0x00)
+        if (node->suc != NULL)
             node->suc->pre = node->pre;
     }
     else
@@ -106,7 +80,6 @@ void list_delete(dl_list_t *list, dl_list_node_t *node)
         list->start = node->suc;
     }
 
-    ysm_free(node, __FILE__, __LINE__);
-    node = NULL;
+    YSM_FREE(node);
     list->length--;
 }
--- src/ylist.h
+++ src/ylist.h
@@ -1,55 +1,28 @@
-/*
--================================ lists.h ===================================-
-
-YSM (YouSickMe) ICQ Client. An Original Multi-Platform ICQ client.
-Copyright (c) 2007 rad2k Argentina.
-Copyright (c) 2008 vs Russia
-
-YSM is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-For Contact information read the AUTHORS file.
-
-
-*/
-
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef _YLIST_H_
+#define _YLIST_H_
 
 #define COMMON_LIST \
-    struct _dl_list_node_t *pre; \
-    struct _dl_list_node_t *suc;
+    struct _list_node_t *pre; \
+    struct _list_node_t *suc;
 
-typedef struct _dl_list_node_t
+typedef struct _list_node_t
 {
     COMMON_LIST
-} dl_list_node_t;
+} list_node_t;
 
 typedef struct
 {
-    dl_list_node_t* start;
-    u_int32_t length;
-} dl_list_t;
+    list_node_t *start;
+    uint32_t     length;
+} list_t;
 
-void        freelist(dl_list_t *list);
-dl_list_node_t * list_unshift(dl_list_t *list, dl_list_node_t *node);
+void         freeList(list_t *list);
+void         deleteListNode(list_t *list, list_node_t *node);
+list_node_t *unshiftListNode(list_t *list, list_node_t *node);
 
-dl_list_node_t * list_insert_after(
-    dl_list_t *list,
-    dl_list_node_t *node,
-    dl_list_node_t *pre);
+list_node_t *insertListNodeAfter(
+    list_t      *list,
+    list_node_t *node,
+    list_node_t *pre);
 
-void        list_delete(dl_list_t *list, dl_list_node_t *node);
-
-#endif
+#endif /* _YLIST_H_ */
